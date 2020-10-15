8255e5cacaf7f3b577510fbb47323cf0923908d8
szymmirr <szymmirr@wp.pl>
2020-10-15 19:37:48 +0200

    Move Mikrofon to upper dir

--- S3/ASK/Mikrofon/MIC/MIC/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// MIC.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
